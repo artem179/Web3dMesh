d643854a5a973ed58ded35b5f4299107a0163bf8
artem179 <artyom179@mail.ru>
2020-07-01 00:08:23 +0300

    Projective camera combined with a three-dimensional mesh and video stream

--- public/sketch.js
+++ public/sketch.js
@@ -13,19 +13,19 @@
 // var VTX = VTX7;
 
 // === important facial feature 33 points ===
-// var VTX = VTX33;
+var VTX = VTX468;
 
 // === standard facial landmark 68 points ===
 // var VTX = VTX68;
 
 // === full facemesh 468 points ===
-var VTX = VTX468;
+// var VTX = VTX468;
 
 // select the right triangulation based on vertices
 var TRI = VTX == VTX7 ? TRI7 : (VTX == VTX33 ? TRI33 : (VTX == VTX68 ? TRI68 : TRI468))
 var UV  = VTX == VTX7 ? UV7  : (VTX == VTX33 ? UV33  : (VTX == VTX68 ? UV68  : UV468))
 
-var MAX_FACES = 1; //default 10
+var MAX_FACES = 4; //default 10
 
 
 var socket = io(); // the networking library
@@ -46,12 +46,12 @@ var faceMeshes = {}; // these are threejs objects that makes up the rendering of
                      // stored as { userId : Array<Object3D> }
 
 // html canvas for drawing debug view
-var dbg = document.createElement("canvas").getContext('2d');
-dbg.canvas.style.position="absolute";
-dbg.canvas.style.left = "0px";
-dbg.canvas.style.top = "0px";
-dbg.canvas.style.zIndex = 100; // "bring to front"
-document.body.appendChild(dbg.canvas);
+// var dbg = document.createElement("canvas").getContext('2d');
+// dbg.canvas.style.position="absolute";
+// dbg.canvas.style.left = "0px";
+// dbg.canvas.style.top = "0px";
+// dbg.canvas.style.zIndex = 100; // "bring to front"
+// document.body.appendChild(dbg.canvas);
 
 
 // boilerplate to initialize threejs scene
@@ -80,10 +80,13 @@ capture.style.zIndex =-100 // "send to back"
 // signal when capture is ready and set size for debug canvas
 capture.onloadeddata = function(){
   console.log("video initialized");
+  if (videoDataLoaded == false) {
+    initVideoplane();
+  }
   videoDataLoaded = true;
-  dbg.canvas.width = capture.videoWidth /2; // half size
-  dbg.canvas.height= capture.videoHeight/2;
-  
+  // dbg.canvas.width = capture.videoWidth /2; // half size
+  // dbg.canvas.height= capture.videoHeight/2;
+
   camera.position.z = capture.videoWidth/2; // rough estimate for suitable camera distance based on FOV
 }
 
@@ -100,7 +103,7 @@ const material = new THREE.MeshNormalMaterial();
 // threejs has a "scene" model, so we don't have to specify what to draw each frame,
 // instead we put objects at right positions and threejs renders them all
 function updateMeshesFromServerData(){
-  
+
   // first, we add the newly appeared faces from the server
   for (var userId in serverData){
     if (!faceMeshes[userId] && serverData[userId].faces.length){
@@ -113,13 +116,13 @@ function updateMeshesFromServerData(){
       }
       for (var i = 0; i < TRI.length; i+=3){
         geom.faces.push(
-          new THREE.Face3(TRI[i], TRI[i+1], TRI[i+2]),
+            new THREE.Face3(TRI[i], TRI[i+1], TRI[i+2]),
         );
         geom.faceVertexUvs[0].push(
-          [ new THREE.Vector2(UV[TRI[i  ]][0],1-UV[TRI[i  ]][1]),
-            new THREE.Vector2(UV[TRI[i+1]][0],1-UV[TRI[i+1]][1]),
-            new THREE.Vector2(UV[TRI[i+2]][0],1-UV[TRI[i+2]][1]),
-          ],
+            [ new THREE.Vector2(UV[TRI[i  ]][0],1-UV[TRI[i  ]][1]),
+              new THREE.Vector2(UV[TRI[i+1]][0],1-UV[TRI[i+1]][1]),
+              new THREE.Vector2(UV[TRI[i+2]][0],1-UV[TRI[i+2]][1]),
+            ],
         )
       }
       var mesh = new THREE.Mesh(geom,material);
@@ -140,7 +143,7 @@ function updateMeshesFromServerData(){
   //   frames and see if it comes back
   // - if one user is gone but another just came in, instead of removing a bunch of meshes and adding
   //   a bunch again, recycle by reassigning the meshes
-  
+
   // update vertices
   for (var userId in faceMeshes){
     if (!serverData[userId]){
@@ -148,21 +151,22 @@ function updateMeshesFromServerData(){
       continue;
     }
     for (var i = 0; i < Math.min(faceMeshes[userId].length, serverData[userId].faces.length); i++){
-      
+
       let geom = faceMeshes[userId][i].geometry;
       let targ = serverData[userId].faces[i].scaledMesh;
       for (var j = 0; j < targ.length; j++){
         var p = webcam2space(...targ[j]);
-        // console.log(p)
+        console.log("Ppp - ", p)
+        console.log("CAMera pos -", camera.position)
         geom.vertices[j].set(p.x,p.y,p.z);
       }
-      
+
       geom.computeVertexNormals();
       // geom.computeFaceNormals();
       geom.verticesNeedUpdate = true;
     }
   }
-  
+
 }
 
 // Load the MediaPipe facemesh model assets.
@@ -182,23 +186,23 @@ socket.on('server-update', function(data){
 })
 
 // draw a face object (2D debug view) returned by facemesh
-function drawFaces(faces,noKeypoints){
-  for (var i = 0; i < faces.length; i++){
-    const keypoints = faces[i].scaledMesh;
+// function drawFaces(faces,noKeypoints){
+//   for (var i = 0; i < faces.length; i++){
+//     const keypoints = faces[i].scaledMesh;
 
-    for (var j = 0; j < TRI.length; j+=3){
-      var a = keypoints[TRI[j  ]];
-      var b = keypoints[TRI[j+1]];
-      var c = keypoints[TRI[j+2]];
-      dbg.beginPath();
-      dbg.moveTo(a[0],a[1]);
-      dbg.lineTo(b[0],b[1]);
-      dbg.lineTo(c[0],c[1]);
-      dbg.closePath();
-      dbg.stroke();
-    }
-  }
-}
+//     for (var j = 0; j < TRI.length; j+=3){
+//       var a = keypoints[TRI[j  ]];
+//       var b = keypoints[TRI[j+1]];
+//       var c = keypoints[TRI[j+2]];
+//       dbg.beginPath();
+//       dbg.moveTo(a[0],a[1]);
+//       dbg.lineTo(b[0],b[1]);
+//       dbg.lineTo(c[0],c[1]);
+//       dbg.closePath();
+//       dbg.stroke();
+//     }
+//   }
+// }
 
 // hash to a unique color for each user ID
 function uuid2color(uuid){
@@ -213,9 +217,9 @@ function uuid2color(uuid){
 // transform webcam coordinates to threejs 3d coordinates
 function webcam2space(x,y,z){
   return new THREE.Vector3(
-     (x-capture.videoWidth /2),
-    -(y-capture.videoHeight/2), // in threejs, +y is up
-    - z
+      (x-capture.videoWidth /2),
+      -(y-capture.videoHeight/2), // in threejs, +y is up
+      - z
   )
 }
 
@@ -238,13 +242,13 @@ function packFace(face,set){
 
 function render() {
   requestAnimationFrame(render); // this creates an infinite animation loop
-    
+
   if (facemeshModel && videoDataLoaded){ // model and video both loaded
     facemeshModel.pipeline.maxFaces = MAX_FACES;
     facemeshModel.estimateFaces(capture).then(function(_faces){
       // we're faceling an async promise
       // best to avoid drawing something here! it might produce weird results due to racing
-      
+
       myFaces = _faces.map(x=>packFace(x,VTX));  // update the global myFaces object with the detected faces
       if (!myFaces.length){
         // haven't found any faces
@@ -253,95 +257,45 @@ function render() {
         // display the confidence, to 3 decimal places
         statusText = "Confidence: "+ (Math.round(myFaces[0].faceInViewConfidence*1000)/1000);
       }
-      
+
       // tell the server about our updates!
       socket.emit('client-update',{faces:myFaces});
     })
   }
 
-  video = document.getElementById( 'video' );
+//   dbg.clearRect(0,0,dbg.canvas.width,dbg.canvas.height);
 
-  var texture = new THREE.VideoTexture( video );
+//   dbg.save();
+//   dbg.fillStyle="red";
+//   dbg.strokeStyle="red";
+//   dbg.scale(0.5,0.5); //halfsize;
 
-  var geometry = new THREE.PlaneBufferGeometry( 16, 9 );
-  geometry.scale( 0.5, 0.5, 0.5 );
-  var material = new THREE.MeshBasicMaterial( { map: texture } );
+//   dbg.drawImage(capture,0,0);
+//   drawFaces(myFaces);
+//   dbg.restore();
 
-  // var count = 128;
-  // var radius = 32;
-  //
-  // for ( var i = 1, l = count; i <= l; i ++ ) {
-  //
-  //   var phi = Math.acos( - 1 + ( 2 * i ) / l );
-  //   var theta = Math.sqrt( l * Math.PI ) * phi;
-  //
-  //   var mesh = new THREE.Mesh( geometry, material );
-  //   mesh.position.setFromSphericalCoords( radius, phi, theta );
-  //   mesh.lookAt( camera.position );
-  //   scene.add( mesh );
-  //
-  // }
+//   dbg.save();
+//   dbg.fillStyle="red";
+//   dbg.fillText(statusText,2,60);
+//   dbg.restore();
 
-  var mesh = new THREE.Mesh( geometry, material );
-  mesh.position.set(0, 0, -5.);
-  mesh.lookAt( camera.position );
-  scene.add( mesh );
-
-  //window.addEventListener( 'resize', onWindowResize, false );
-
-  //
-
-  if ( navigator.mediaDevices && navigator.mediaDevices.getUserMedia ) {
-
-    var constraints = { video: { width: 1280, height: 720, facingMode: 'user' } };
-
-    navigator.mediaDevices.getUserMedia( constraints ).then( function ( stream ) {
-
-      // apply the stream to the video element used in the texture
-
-      video.srcObject = stream;
-      video.play();
-
-    } ).catch( function ( error ) {
-
-      console.error( 'Unable to access the camera/webcam.', error );
-
-    } );
-
-  } else {
-
-    console.error( 'MediaDevices interface not available.' );
-
-  }
-
-  function onWindowResize() {
-
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( window.innerWidth, window.innerHeight );
-
-  }
-
-  
-  dbg.clearRect(0,0,dbg.canvas.width,dbg.canvas.height);
-  
-  dbg.save();
-  dbg.fillStyle="red";
-  dbg.strokeStyle="red";
-  dbg.scale(0.5,0.5); //halfsize;
-  
-  dbg.drawImage(capture,0,0);
-  drawFaces(myFaces);
-  dbg.restore();
-  
-  dbg.save();
-  dbg.fillStyle="red";
-  dbg.fillText(statusText,2,60);
-  dbg.restore();
-  
   // render the 3D scene!
   renderer.render( scene, camera );
+}
+function initVideoplane() {
+  var videoTexture = new THREE.VideoTexture(capture);
+
+  var videoGeometry = new THREE.PlaneBufferGeometry( capture.videoWidth, capture.videoHeight );
+  console.log("W,h", capture.videoWidth, capture.videoHeight);
+  //videoGeometry.scale( 0.5, 0.5, 0.5 );
+  var videoMaterial = new THREE.MeshBasicMaterial( { map: videoTexture } );
+  console.log("Camera position - ", camera.position.z)
+  console.log("CAMERA - ", camera)
+
+  var videoMesh = new THREE.Mesh( videoGeometry, videoMaterial );
+  //videoMesh.position.set(0, 0, 310);
+  //mesh.lookAt( camera.position );
+  scene.add( videoMesh );
 }
 
 render(); // kick off the rendering loop!
